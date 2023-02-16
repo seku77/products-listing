367654fb8dee1d49bb6146b08a9e6d63f7154779
Bartłomiej Sęk <bartek.sek77@gmail.com>
2023-02-16 22:36:53 +0100

    Moved global css to separate file

--- src/App.vue
+++ src/App.vue
@@ -14,32 +14,5 @@ export default {
 </script>
 
 <style lang="scss">
-* {
-  box-sizing: border-box;
-}
-body {
-  margin: 0;
-}
-.badges {
-  display: flex;
-  gap: 8px;
-}
-.badge {
-  height: 24px;
-  width: 24px;
-  background-repeat: no-repeat;
-  background-position: center;
-  &.new {
-    background-image: url("./assets/icons/new.svg");
-  }
-  &.collectionSale {
-    background-image: url("./assets/icons/tag.svg");
-  }
-  &.lastItems {
-    background-image: url("./assets/icons/cart-exclamation.svg");
-  }
-  &.ratingStar {
-    background-image: url("./assets/icons/star.svg");
-  }
-}
+@import "./style/global.scss";
 </style>
